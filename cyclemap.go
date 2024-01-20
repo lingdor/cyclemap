adfd0ed3fe8bb1cedf1d49a7afda9ffc72c2cbd7
zhangxiaoxu <31872163@qq.com>
2024-01-21 02:37:06 +0800

    add go.mod

--- cyclemap.go
+++ cyclemap.go
@@ -14,13 +14,39 @@ type CycleMap[K cmp.Ordered, V any] struct {
 	size   int
 }
 
+func (c *CycleMap[K, V]) GetOrAdd(k K, f func() V) V {
+	if c.isSafe {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+	}
+	if v, ok := c.get(k); ok {
+		return v
+	}
+	v := f()
+	c.set(k, v)
+	return v
+}
+
 func (c *CycleMap[K, V]) Get(k K) (V, bool) {
 	if c.isSafe {
 		c.mu.Lock()
 		defer c.mu.Unlock()
 	}
+	return c.get(k)
+}
+func (c *CycleMap[K, V]) get(k K) (V, bool) {
 	v, ok := c.mapVal[k]
 	return v, ok
+}
+func (c *CycleMap[K, V]) Remove(k K) {
+	if c.isSafe {
+		c.mu.Lock()
+		defer c.mu.Unlock()
+	}
+	c.remove(k)
+}
+func (c *CycleMap[K, V]) remove(k K) {
+	delete(c.mapVal, k)
 }
 
 func (c *CycleMap[K, V]) Set(k K, v V) {
@@ -28,6 +54,10 @@ func (c *CycleMap[K, V]) Set(k K, v V) {
 		c.mu.Lock()
 		defer c.mu.Unlock()
 	}
+	c.set(k, v)
+}
+
+func (c *CycleMap[K, V]) set(k K, v V) {
 	if _, ok := c.mapVal[k]; ok {
 		c.mapVal[k] = v
 		return
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lingdor/cyclemap
+
+go 1.21.4
